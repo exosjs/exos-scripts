5dc05313d103e590b386c556868b67ace6c88b1c
onatolich <onatolich@gmail.com>
2020-07-22 13:25:02 -0700

    Fix import resolver
    Signed-off-by: onatolich <onatolich@gmail.com>

--- src/scripts/lint/.eslintrc.library.js
+++ src/scripts/lint/.eslintrc.library.js
@@ -1,8 +1,8 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   extends: [
-    "eslint-config-airbnb",
-    "plugin:@typescript-eslint/recommended",
+    'eslint-config-airbnb',
+    'plugin:@typescript-eslint/recommended',
   ],
   env: {
     node: true,
@@ -10,21 +10,29 @@ module.exports = {
   },
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module", // Allows for the use of imports
+    sourceType: 'module', // Allows for the use of imports
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
   },
   plugins: [
     '@typescript-eslint',
     'import',
   ],
   rules: {
-    "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/no-empty-function": ["error", { allow: ["arrowFunctions"] }],
-    "no-prototype-builtins": "off",
-    "object-curly-newline": ["error", {
-      "ObjectExpression": { "multiline": true, "consistent": true },
-      "ObjectPattern": { "multiline": true, "consistent": true },
-      "ImportDeclaration": { "multiline": true, "consistent": true },
-      "ExportDeclaration": { "multiline": true, "consistent": true }
-    }]
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-empty-function': ['error', { allow: ['arrowFunctions'] }],
+    'import/extensions': ['error', { js: 'never', jsx: 'never', ts: 'never', tsx: 'never' }],
+    'no-prototype-builtins': 'off',
+    'object-curly-newline': ['error', {
+      ObjectExpression: { multiline: true, consistent: true },
+      ObjectPattern: { multiline: true, consistent: true },
+      ImportDeclaration: { multiline: true, consistent: true },
+      ExportDeclaration: { multiline: true, consistent: true },
+    }],
   },
 };
