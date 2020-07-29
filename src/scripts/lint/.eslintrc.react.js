7aef10b38bfe9400e94a2e3d2aeb9eafe2b59349
onatolich <onatolich@gmail.com>
2020-07-29 11:22:00 -0700

    fix: fix lint rules for containers files

--- src/scripts/lint/.eslintrc.react.js
+++ src/scripts/lint/.eslintrc.react.js
@@ -98,5 +98,11 @@ module.exports = {
         'react/jsx-no-bind': 'off',
       },
     },
+    {
+      files: ['*.container.*'],
+      rules: {
+        'react/jsx-handler-names': 'off',
+      },
+    },
   ],
 };
