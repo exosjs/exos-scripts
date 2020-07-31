e043f8fc4cb90069483faef78f23b167969451ed
onatolich <onatolich@gmail.com>
2020-07-31 15:04:06 -0700

    feat: update tests to run without need to build them before

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 #!/usr/bin/env node
 import exosScripts from './exos-scripts';
 
-const scriptToExecute = process.argv[2];
-const otherArgs = process.argv.slice(3);
-
-exosScripts(scriptToExecute, otherArgs);
+exosScripts(process.argv[2]);
